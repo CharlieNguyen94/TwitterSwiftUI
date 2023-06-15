324b0d008ccaf922041d2f5cbca961a65d72e679
Charlie <charlietunguyen@hotmail.com>
2023-06-15 17:22:26 +0100

    Initial Commit

---
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIApp.swift
+//  TwitterSwiftUI
+//
+//  Created by Charlie Nguyen on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
