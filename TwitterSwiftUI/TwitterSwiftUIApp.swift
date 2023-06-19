37d77ebda268c4d987c9c7e2d6e6379f1577bc79
Charlie <charlietunguyen@hotmail.com>
2023-06-19 15:55:25 +0100

    Firebase Setup / Integration

--- TwitterSwiftUI/TwitterSwiftUIApp.swift
+++ TwitterSwiftUI/TwitterSwiftUIApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import Firebase
 
 @main
 struct TwitterSwiftUIApp: App {
+
+	init() {
+		FirebaseApp.configure()
+	}
+
     var body: some Scene {
 		WindowGroup {
 			NavigationStack {
